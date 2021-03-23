9e2b8e81e569a17b84b2822461c32a654ea5a91d
Chris Essoe <chrisessoe@gmail.com>
2021-03-23 15:40:48 -0700

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -29,11 +29,9 @@ class CommentForm extends Component {
         });
     }
     
-handleSubmit(values) {
-        console.log(JSON.stringify(values));
-        alert(JSON.stringify(values));
+    handleSubmit(values) {
         this.toggleModal();
-
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -113,7 +111,7 @@ function RenderCampsite({campsite}) {
         );
     }
     
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="col-5-md m-1">
@@ -125,7 +123,7 @@ function RenderComments({comments}) {
                             </div>
                         )
                     })}
-                    <CommentForm />
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
                 </div>
             );
         }
@@ -147,7 +145,11 @@ function RenderComments({comments}) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
